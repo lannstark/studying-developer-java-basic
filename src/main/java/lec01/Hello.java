75b2fe5f0ae7da459f60b088c6f7e0de1f0ea62e
lannstark <choi-0202@kaist.ac.kr>
2020-02-02 22:40:34 +0900

    1강. Hello World

---
+++ src/main/java/lec01/Hello.java
@@ -0,0 +1,30 @@
+// 당연히 한글을 막 쓰면 빨간줄이 나와요!
+// 주석 : 컴퓨터가 무시하는 문장 (//를 사용)
+// 주석처리 : 어떤 문장을 주석으로 만든다
+
+/*
+ 여러 줄 주석 처리하기
+ 한 문장이 아니라 여러 줄을 주석으로 만들 수 있어요!
+ */
+
+// package : 폴더
+package lec01;
+
+public class Hello {
+
+    // 여기서부터 프로그램이 시작해요 ~~~
+    public static void main(String[] args) {
+        // 여기서 프로그램을 작성할거에요!
+        // 오늘은 출력을 해봅시다!
+        // System.out.println() : () 안에 있는 문장을 출력해 주세요!
+        // 주의할 점 : 문장은 " " 를 써야 한다.
+        // ( ) 안에 문장을 자유롭게 적으시면 됩니다
+        System.out.println("Hello World");
+        System.out.println("아 자바 너무 어려워");
+        System.out.println("도대체 자바 강의는 언제 끝날까?");
+
+
+        System.out.println("안녕 나야~");
+    }
+
+}
