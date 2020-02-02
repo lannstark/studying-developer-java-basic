3d629f3421dd86e8d46b9313634cf9440d84451e
lannstark <choi-0202@kaist.ac.kr>
2020-02-03 00:02:53 +0900

    2강. 숫자, 문자, 특수문자 출력하기

---
+++ src/main/java/lec02/Hello2.java
@@ -0,0 +1,21 @@
+package lec02;
+
+public class Hello2 {
+
+    public static void main(String[] args) {
+        // 컴퓨터는 바보다
+        // 컴퓨터는 바보라서 큰 따옴표(")가 있으면 얘는 '문장'이라고 생각합니다
+        // 큰 따옴표(")가 없으면 숫자라고 생각한다.
+        // 숫자에도 종류가 있다. 정수, 실수, 음수
+        System.out.println(-100);
+
+        // 컴퓨터는 바보라서 문장이랑 문자를 구별합니다. 작은 따옴표(') 문자
+        System.out.println('C');
+
+        // 특수 문자 역슬래시(\)를 사용한다 큰 따옴표 : \"
+        // \n을 사용하면 엔터를 칠 수 있습니다.
+        // \t : 탭, \b : 백스페이스
+        System.out.println(" \" ");
+    }
+
+}
